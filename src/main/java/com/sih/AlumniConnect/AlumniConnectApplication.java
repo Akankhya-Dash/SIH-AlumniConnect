a5d756d16ebdb663aae4c0a738f7c20b4b295e30
Akankhya Dash <akankhyapvt@gmail.com>
2025-09-03 08:54:23 +0530

    Add initial project files and structure

---
+++ src/main/java/com/sih/AlumniConnect/AlumniConnectApplication.java
@@ -0,0 +1,13 @@
+package com.sih.AlumniConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniConnectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sih/AlumniConnect/AlumniRegistration.java
@@ -0,0 +1,5 @@
+package com.sih.AlumniConnect;
+
+public class AlumniRegistration {
+
+}
